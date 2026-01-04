46beaa9aa77a2d5621a67da4f68dd03a877243f6
charlesmatte <matte.charles@gmail.com>
2026-01-04 11:03:53 -0500

    feat: add account management page with user information update functionality

--- notibac/website/views.py
+++ notibac/website/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
 
 def home(request):
@@ -8,3 +9,15 @@ def home(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+@login_required
+def account(request):
+    if request.method == "POST":
+        user = request.user
+        user.first_name = request.POST.get("first_name", "").strip()
+        user.last_name = request.POST.get("last_name", "").strip()
+        user.save()
+        messages.success(request, "Vos informations ont été mises à jour.")
+        return redirect("account")
+    return render(request, "account.html")
